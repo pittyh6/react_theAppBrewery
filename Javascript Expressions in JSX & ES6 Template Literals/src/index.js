03669f0f5c8ce70bee4c1170550b47f37f298d47
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-25 17:43:24 +1000

    add js expression learn

--- Javascript Expressions in JSX & ES6 Template Literals/src/index.js
+++ Javascript Expressions in JSX & ES6 Template Literals/src/index.js
@@ -1,4 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+
+const fName = "Priscila";
+const lName = "F.B.";
+const luckNumber = 5;
+ReactDOM.render(
+    <div>
+        <h1>
+            Hello {fName + " " + lName} OR {fName} {lName} OR {`${fName} ${lName}`}!
+        </h1>
+        <p>My luck number is {luckNumber}</p>
+        <h6>What is your luck number {Math.floor(Math.random() * 10)}</h6>
+    </div>,
+    document.getElementById("root")
+)
