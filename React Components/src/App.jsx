11d74bfbaf21afef7520ff7396e7d892e98ca7e5
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-06-29 20:00:30 +1000

    react components

---
+++ React Components/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import List from "./List";
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
