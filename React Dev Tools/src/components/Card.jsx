959473461ff414e274de345ae7b6d7f183dd6603
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-07-02 16:55:38 +1000

    react dev tools

---
+++ React Dev Tools/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+import Details from "./Details";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Details detailInfo={props.tel} />
+        <Details detailInfo={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
