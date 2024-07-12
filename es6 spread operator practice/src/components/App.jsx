8821a0f7c23a21120545ae5471c250d0a66d8190
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-07-12 17:07:59 +1000

    spread practice done

--- es6 spread operator practice/src/components/App.jsx
+++ es6 spread operator practice/src/components/App.jsx
@@ -4,18 +4,24 @@ import React, { useState } from "react";
 //2. When the add button is pressed, the current data in the input should be
 //added to an array.
 //3. The <ul> should display all the array items as <li>s
-var elementItem = [];
+
+//var elementItem = [];
 function App() {
-  const [item, setItem] = useState("Insert Item");
+  const [item, setItem] = useState("");
+  const [elementItem, setElementItem] = useState([]); //instead of create [] outside, create it as a useState
 
   function getItem(event) {
-    const { value, name } = event.target;
-    setItem(value);
+    //const { value, name } = event.target;
+    const newValueInput = event.target.value;
+    setItem(newValueInput);
   }
 
   function addItem() {
-    elementItem.unshift(item);
-    console.log(elementItem);
+    //elementItem.unshift(item);
+    setElementItem((prevItemEl) => {
+      return [...prevItemEl, item];
+    });
+    setItem("");
   }
   return (
     <div className="container">
@@ -23,16 +29,16 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" onChange={getItem} />
+        <input type="text" onChange={getItem} value={item} />
         <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          {elementItem.map((el) => {
-            return <li>{el}</li>;
-          })}
+          {elementItem.map((el) => (
+            <li>{el}</li>
+          ))}
         </ul>
       </div>
     </div>
